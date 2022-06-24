fa2e45234cb589d0892535fbfe9458ece3895aee
Katarina Anton <kaciak.maciak@gmail.comcd>
2022-06-24 15:19:00 +1200

    feat: add interval data

--- App.tsx
+++ App.tsx
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import {StyleSheet, Text, View} from 'react-native'
+import {QueryClientProvider, QueryClient} from 'react-query'
+import {useSubscription} from 'react-query-subscription'
+import {interval} from 'rxjs'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <ExampleInterval />
+      </View>
+    </QueryClientProvider>
+  )
+}
+
+const interval$ = interval(1000)
+
+function ExampleInterval() {
+  const {data, isLoading, isError, error} = useSubscription(
+    'example-key',
+    () => interval$,
+  )
+
+  if (isLoading) {
+    return <Text>Loading...</Text>
+  }
+  if (isError) {
+    return <Text>{error?.message || 'Unknown error'}</Text>
+  }
+  return <Text>Data: {JSON.stringify(data)}</Text>
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +41,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
